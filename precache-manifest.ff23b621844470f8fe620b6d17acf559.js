e38b8f96dbec2659314a0ecdf66b9f7c83042aa5
stasi815 <anastasia.gallardo@students.makeschool.com>
2020-05-12 11:21:40 -0700

    Updates

---
+++ precache-manifest.ff23b621844470f8fe620b6d17acf559.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c04d5481339da2c5b880c4c7bfe844",
+    "url": "/AG-FEW-Final-Assessment/index.html"
+  },
+  {
+    "revision": "1b7280f59777e65fb645",
+    "url": "/AG-FEW-Final-Assessment/static/css/main.16380e40.chunk.css"
+  },
+  {
+    "revision": "8a3b13e8e674dd6158da",
+    "url": "/AG-FEW-Final-Assessment/static/js/2.8e507459.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AG-FEW-Final-Assessment/static/js/2.8e507459.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b7280f59777e65fb645",
+    "url": "/AG-FEW-Final-Assessment/static/js/main.361f6e3c.chunk.js"
+  },
+  {
+    "revision": "3c6fe1cd3a9548b79389",
+    "url": "/AG-FEW-Final-Assessment/static/js/runtime-main.9a43e72f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AG-FEW-Final-Assessment/static/media/logo.5d5d9eef.svg"
+  }
+]);
